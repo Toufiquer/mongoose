2d47d5fd5affdbc6125ddfb4537284e62dc7cf7e
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-05-24 23:51:46 +0600

    add video-1 dome

---
+++ video-1/index.ts
@@ -0,0 +1,38 @@
+/* *  Intro to MongoDB *  */
+//// module 6
+//& video 1
+/**
+ *@ Mongoose  is an object Data Modeling (ODM) library for MongoDB
+ *@ It provides a higher level abstraction for interacting with mongodb. Making it easier to create or work with data.
+ *@ With Mongoose you can define schema and models and relation ship for you mongodb document.
+ * */
+
+/**
+ * # MongoDB
+ * find()
+ * find()
+ * insertMany()
+ * count()
+ * update()
+ * delete()
+ * (etc...)
+ *
+ * # Mongoose
+ * save()
+ * populate()
+ * Statics
+ * Instance Methods
+ * */
+
+/**
+ * # Key Features of MongoDB
+ * Schema Definition
+ * Model Creation
+ * Data Validation
+ * Querying
+ * Middleware Support
+ * Population
+ *
+ * ### Interface => Schema => Model => Database Query
+ *
+ * */
